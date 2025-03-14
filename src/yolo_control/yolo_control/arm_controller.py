a26a061567731713b3a061307562da3504516623
MoRan <ooklibaioo@gmail.com>
2025-03-14 21:09:36 +0800

    Update arm_controller.py

--- src/yolo_control/yolo_control/arm_controller.py
+++ src/yolo_control/yolo_control/arm_controller.py
@@ -22,9 +22,9 @@ class ArmController:
     def pick_up(self):
         # 示例：机械臂拾取动作
         # 你需要根据实际情况调整这些位置和时间
-        self.move_to_position([[1, 90], [2, 0], [3, 55], [4, 45], [5, 90], [6, 0]], duration=1)
+        self.move_to_position([[1, 90], [2, 0], [3, 45], [4, 45], [5, 90], [6, 0]], duration=1)
         time.sleep(1)
-        self.move_to_position([[1, 90], [2, 25], [3, 55], [4, 45], [5, 45], [6, 0]], duration=1)
+        self.move_to_position([[1, 90], [2, 0], [3, 60], [4, 45], [5, 45], [6, 0]], duration=1)
         time.sleep(1)
         self.move_to_position([[1, 90], [2, 25], [3, 60], [4, 45], [5, 90], [6, 90]], duration=1)
         time.sleep(1)
