196394193e7c5c0d519f1236f8f27e1f609ac5a6
MoRan <ooklibaioo@gmail.com>
2025-03-22 01:03:21 +0800

    Update nav2_launch.py

--- src/nav2_ros2/launch/nav2_launch.py
+++ src/nav2_ros2/launch/nav2_launch.py
@@ -10,21 +10,50 @@ def generate_launch_description():
     map_file = os.path.join(get_package_share_directory('nav2_ros2'), 'maps', 'rm349.yaml')
 
     return LaunchDescription([
+        # 地图服务器
         Node(
             package='nav2_map_server',
             executable='map_server',
             name='map_server',
             output='screen',
             parameters=[{'use_sim_time': use_sim_time}, 
-                        {'yaml_filename':map_file} 
+                        {'yaml_filename': map_file} 
                        ]),
 
+        # 路径规划器
+        Node(
+            package='nav2_planner',
+            executable='planner_server',
+            name='planner_server',
+            output='screen',
+            parameters=[{'use_sim_time': use_sim_time}]
+        ),
+
+        # 控制器服务器
+        Node(
+            package='nav2_controller',
+            executable='controller_server',
+            name='controller_server',
+            output='screen',
+            parameters=[{'use_sim_time': use_sim_time}]
+        ),
+
+        # AMCL（自适应蒙特卡洛定位）
+        Node(
+            package='nav2_amcl',
+            executable='amcl',
+            name='amcl',
+            output='screen',
+            parameters=[{'use_sim_time': use_sim_time}]
+        ),
+
+        # 生命周期管理器
         Node(
             package='nav2_lifecycle_manager',
             executable='lifecycle_manager',
-            name='lifecycle_manager_mapper',
+            name='lifecycle_manager_navigation',
             output='screen',
             parameters=[{'use_sim_time': use_sim_time},
                         {'autostart': True},
-                        {'node_names': ['map_server']}])            
-        ])
+                        {'node_names': ['map_server', 'planner_server', 'controller_server', 'amcl']}])
+    ])
