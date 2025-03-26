22d2c0dab12155d58a9cdf3e21be9954f1f91364
MoRan <ooklibaioo@gmail.com>
2025-03-27 02:12:24 +0800

    Update driver.launch.py

--- src/driver_ros2/launch/driver.launch.py
+++ src/driver_ros2/launch/driver.launch.py
@@ -13,6 +13,38 @@ def generate_launch_description():
         parameters = [{'use_sim_time': use_sim_time}],
         output='screen',
         )
+    
+    motor_control_node = Node(
+        package='driver_ros2',
+        executable='motor_control_node',
+        name='motor_control_node',
+        parameters = [{'use_sim_time': use_sim_time}],
+        output='screen',
+    )
+    
+    battery_status_node = Node(
+        package='driver_ros2',
+        executable='battery_status_node',
+        name='battery_status_node',
+        parameters = [{'use_sim_time': use_sim_time}],
+        output='screen',
+    )
+
+    twist_motor_control_node = Node(
+        package='driver_ros2',
+        executable='twist_motor_control_node',
+        name='twist_motor_control_node',
+        parameters = [{'use_sim_time': use_sim_time}],
+        output='screen',
+    )
+
+    pwm_servo_control_node = Node(
+        package='driver_ros2',
+        executable='pwm_servo_control_node',
+        name='pwm_servo_control_node',
+        parameters = [{'use_sim_time': use_sim_time}],
+        output='screen',
+    )
 
     return LaunchDescription([
         DeclareLaunchArgument(
@@ -20,4 +52,8 @@ def generate_launch_description():
             default_value='false',
             description='Use simulation (Gazebo) clock if true'),
         driver_node,
+        motor_control_node,
+        battery_status_node,
+        twist_motor_control_node,
+        pwm_servo_control_node,
     ])
