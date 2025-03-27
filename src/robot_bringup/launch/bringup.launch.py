f9d2df938f66e845c744ccb741b92ca199e3d5ec
MoRan1412 <ooklibaioo@gmail.com>
2025-03-27 21:30:31 +0800

    bringup.py

---
+++ src/robot_bringup/launch/bringup.launch.py
@@ -0,0 +1,35 @@
+import launch
+import launch_ros
+from ament_index_python.packages import get_package_share_directory
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+def generate_launch_description():
+    bot_bringup_dir = get_package_share_directory(
+        'robot_bringup')
+    lidar_ros2_dir = get_package_share_directory(
+        'sllidar_ros2')
+
+    urdf2tf = launch.actions.IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [bot_bringup_dir, '/launch', '/urdf2tf.launch.py']),
+    )
+
+    odom2tf = launch_ros.actions.Node(
+        package='robot_bringup',
+        executable='odom2tf',
+        output='screen'
+    )
+
+    lidar = launch.actions.IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [lidar_ros2_dir, '/launch', '/sllidar_c1_launch.py']),
+    )
+
+    # 使用 TimerAction 启动后 5 秒执行 ydlidar 节点
+    lidar_delay = launch.actions.TimerAction(period=5.0, actions=[lidar])
+    
+    return launch.LaunchDescription([
+        urdf2tf,
+        odom2tf,
+        lidar_delay
+    ])
