658ecc1d74da2197d7b412d23c22ed2230769338
MoRan1412 <ooklibaioo@gmail.com>
2025-03-31 01:59:30 +0800

    camera stream

---
+++ src/camera_stream/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+package_name = 'camera_stream'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='macvmccw',
+    maintainer_email='ooklibaioo@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'camera_publisher = camera_stream.camera_publisher:main',
+            'camera_subscriber = camera_stream.camera_subscriber:main',
+        ],
+    },
+)
