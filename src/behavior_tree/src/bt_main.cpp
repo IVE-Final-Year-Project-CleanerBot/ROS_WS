06128f4dda5ba5ea0092e939342ad812a51e42ce
MoRan <ooklibaioo@gmail.com>
2025-04-13 23:52:12 +0800

    BT

--- src/behavior_tree/src/bt_main.cpp
+++ src/behavior_tree/src/bt_main.cpp
@@ -1,37 +1,28 @@
+#include <behaviortree_cpp_v3/bt_factory.h>
+#include <rclcpp/rclcpp.hpp>
 #include "behavior_tree/behavior_tree_nodes.hpp"
 
-// 检测瓶子节点实现
-BT::NodeStatus CheckForBottles::tick() {
-  auto request = std::make_shared<std_srvs::srv::SetBool::Request>();
-  request->data = true;
+int main(int argc, char** argv) {
+  rclcpp::init(argc, argv);
+  auto node = rclcpp::Node::make_shared("behavior_tree_node");
 
-  if (!client_->wait_for_service(std::chrono::seconds(1))) {
-    RCLCPP_ERROR(node_->get_logger(), "Service not available");
-    return BT::NodeStatus::FAILURE;
+  BT::BehaviorTreeFactory factory;
+
+  // 注册自定义节点
+  factory.registerNodeType<CheckForBottles>("CheckForBottles");
+  factory.registerNodeType<ControlArm>("ControlArm");
+
+  // 加载行为树 XML 文件
+  std::string tree_file = ament_index_cpp::get_package_share_directory("behavior_tree") + "/config/recycle_bt.xml";
+  auto tree = factory.createTreeFromFile(tree_file);
+
+  // 执行行为树
+  while (rclcpp::ok()) {
+    tree.tickRoot();
+    rclcpp::spin_some(node);
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
   }
 
-  auto result = client_->async_send_request(request);
-  if (rclcpp::spin_until_future_complete(node_, result) ==
-      rclcpp::FutureReturnCode::SUCCESS) {
-    bool detected = result.get()->success;
-    setOutput("detected", detected);
-    return detected ? BT::NodeStatus::SUCCESS : BT::NodeStatus::FAILURE;
-  }
-
-  return BT::NodeStatus::FAILURE;
-}
-
-// 控制机械臂节点实现
-BT::NodeStatus ControlArm::tick() {
-  auto action = getInput<std::string>("action");
-  if (!action) {
-    throw BT::RuntimeError("Missing required input [action]");
-  }
-
-  auto msg = std_msgs::msg::String();
-  msg.data = action.value();
-  publisher_->publish(msg);
-
-  RCLCPP_INFO(node_->get_logger(), "Sent arm command: %s", action.value().c_str());
-  return BT::NodeStatus::SUCCESS;
+  rclcpp::shutdown();
+  return 0;
 }
