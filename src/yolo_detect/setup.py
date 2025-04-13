9b16f1a1346859221a9deca053b0a8455263ec9d
MoRan <ooklibaioo@gmail.com>
2025-04-14 02:54:14 +0800

    Update setup.py

--- src/yolo_detect/setup.py
+++ src/yolo_detect/setup.py
@@ -29,6 +29,7 @@ setup(
     entry_points={
         'console_scripts': [
             'yolo_detect_node = yolo_detect.yolo_detect_node:main',
+            'navigate_node = yolo_detect.navigate_node:main',
         ],
     },
 )
