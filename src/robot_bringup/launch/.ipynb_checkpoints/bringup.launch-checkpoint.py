986e3bd0facd1cf02dbd8255d95b0204bade087e
MoRan <ooklibaioo@gmail.com>
2025-05-08 14:37:22 +0800

    lidar

---
+++ src/robot_bringup/launch/.ipynb_checkpoints/bringup.launch-checkpoint.py
@@ -0,0 +1,62 @@
+import launch
+import launch_ros
+from ament_index_python.packages import get_package_share_directory
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+def generate_launch_description():
+    bot_bringup_dir = get_package_share_directory(
+        'robot_bringup')
+    lidar_ros2_dir = get_package_share_directory(
+        'sllidar_ros2')
+    camera_stream_dir = get_package_share_directory(
+        'camera_stream')
+    driver_ros2_dir = get_package_share_directory('driver_ros2')
+
+    # 启动 URDF 转换
+    urdf2tf = launch.actions.IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [bot_bringup_dir, '/launch', '/urdf2tf.launch.py']),
+    )
+
+    # 启动里程计到 TF 的转换
+    odom2tf = launch_ros.actions.Node(
+        package='robot_bringup',
+        executable='odom2tf',
+        output='screen'
+    )
+
+    # map2odomtf = launch_ros.actions.Node(
+    #     package='robot_bringup',
+    #     executable='map2odomtf',
+    #     output='screen'
+    # )
+
+    # 启动激光雷达
+    lidar = launch.actions.IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [lidar_ros2_dir, '/launch', '/sllidar_c1_launch.py']),
+    )
+
+    # 使用 TimerAction 启动后 5 秒执行 lidar 节点
+    lidar_delay = launch.actions.TimerAction(period=5.0, actions=[lidar])
+
+    # 启动摄像头发布节点
+    camera_publisher = launch.actions.IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [camera_stream_dir, '/launch', '/camera_publisher.launch.py']),
+    )
+
+    # 启动Driver节点
+    driver_nodes = launch.actions.IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [driver_ros2_dir, '/launch', '/driver.launch.py']),
+    )
+    
+    return launch.LaunchDescription([
+        urdf2tf,
+        odom2tf,
+        # map2odomtf,
+        lidar_delay,
+        camera_publisher,
+        driver_nodes,
+    ])
---
+++ src/robot_description/launch/.ipynb_checkpoints/display_robot.launch-checkpoint.py
@@ -0,0 +1,50 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch.substitutions import Command
+from launch_ros.parameter_descriptions import ParameterValue
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+
+    # 获取 URDF 文件路径
+    urdf_package_path = get_package_share_directory('robot_description')
+    default_urdf_path = os.path.join(urdf_package_path, 'urdf', 'robot.urdf')
+    default_rviz_config_path = os.path.join(urdf_package_path, 'config', 'display_robot_model.rviz')
+
+    # Get model urdf content
+    substitutions_command_result = Command(['xacro ', LaunchConfiguration('model')])
+    robot_description_value = ParameterValue(substitutions_command_result, value_type=str)
+
+    robot_state_publisher = Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            parameters=[{'robot_description': robot_description_value}]
+        )
+    
+    joint_state_publisher = Node(
+            package='joint_state_publisher',
+            executable='joint_state_publisher'
+        )
+    
+    rviz2_launch = Node(
+        package='rviz2',
+        executable='rviz2',
+        arguments=['-d', default_rviz_config_path]
+    )
+    
+
+    # 启动 robot_state_publisher 节点
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'model',
+            default_value=str(default_urdf_path),
+            description='Loading model file path'),
+        
+        robot_state_publisher,
+        joint_state_publisher,
+        rviz2_launch
+    ])
