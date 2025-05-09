a9182adb58c60de617206355b907b488db755fcb
MoRan1412 <ooklibaioo@gmail.com>
2025-05-10 00:26:27 +0800

    bringup

--- src/robot_bringup/launch/vm_nav_bringup.launch.py
+++ src/robot_bringup/launch/vm_nav_bringup.launch.py
@@ -5,7 +5,14 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 def generate_launch_description():
     nav2_dir = get_package_share_directory('nav2_ros2')
-    nav2_commander_dir = get_package_share_directory('nav2_commander')
+    yolo_detect_dir = get_package_share_directory('yolo_detect')
+    # nav2_commander_dir = get_package_share_directory('nav2_commander')
+
+    yolo_detect = launch.actions.IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [yolo_detect_dir, '/launch', '/yolo_launch.py']),
+    )
+    yolo_detect_delay = launch.actions.TimerAction(period=5.0, actions=[yolo_detect])
 
     nav2 = launch.actions.IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
@@ -13,13 +20,14 @@ def generate_launch_description():
     )
     nav2_delay = launch.actions.TimerAction(period=5.0, actions=[nav2])
 
-    nav2_commander = launch.actions.IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            [nav2_commander_dir, '/launch', '/nav2_commander_launch.py']),
-    )
-    nav2_commander_delay = launch.actions.TimerAction(period=5.0, actions=[nav2_commander])
+    # nav2_commander = launch.actions.IncludeLaunchDescription(
+    #     PythonLaunchDescriptionSource(
+    #         [nav2_commander_dir, '/launch', '/nav2_commander_launch.py']),
+    # )
+    # nav2_commander_delay = launch.actions.TimerAction(period=5.0, actions=[nav2_commander])
     
     return launch.LaunchDescription([
+        yolo_detect_delay,
         nav2_delay,
-        nav2_commander_delay,
+        # nav2_commander_delay,
     ])
