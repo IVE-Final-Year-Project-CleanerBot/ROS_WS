199af50ed8bde92c31df37ffa380488f3a6c1d96
MoRan1412 <ooklibaioo@gmail.com>
2025-05-10 00:07:48 +0800

    bringup

--- src/robot_bringup/launch/vm_nav_bringup.launch.py
+++ src/robot_bringup/launch/vm_nav_bringup.launch.py
@@ -4,7 +4,14 @@ from ament_index_python.packages import get_package_share_directory
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 def generate_launch_description():
+    nav2_dir = get_package_share_directory('nav2_ros2')
     nav2_commander_dir = get_package_share_directory('nav2_commander')
+
+    nav2 = launch.actions.IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [nav2_dir, '/launch', '/navigation_launch.py']),
+    )
+    nav2_delay = launch.actions.TimerAction(period=5.0, actions=[nav2])
 
     nav2_commander = launch.actions.IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
@@ -13,5 +20,6 @@ def generate_launch_description():
     nav2_commander_delay = launch.actions.TimerAction(period=5.0, actions=[nav2_commander])
     
     return launch.LaunchDescription([
+        nav2_delay,
         nav2_commander_delay,
     ])
--- src/robot_bringup/launch/vm_yolo_bringup.launch.py
+++ src/robot_bringup/launch/vm_yolo_bringup.launch.py
@@ -4,14 +4,7 @@ from ament_index_python.packages import get_package_share_directory
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 def generate_launch_description():
-    nav2_dir = get_package_share_directory('nav2_ros2')
     yolo_detect_dir = get_package_share_directory('yolo_detect')
-
-    nav2 = launch.actions.IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            [nav2_dir, '/launch', '/navigation_launch.py']),
-    )
-    nav2_delay = launch.actions.TimerAction(period=5.0, actions=[nav2])
 
     yolo_detect = launch.actions.IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
@@ -20,6 +13,5 @@ def generate_launch_description():
     yolo_detect_delay = launch.actions.TimerAction(period=5.0, actions=[yolo_detect])
     
     return launch.LaunchDescription([
-        nav2_delay,
         yolo_detect_delay,
     ])
