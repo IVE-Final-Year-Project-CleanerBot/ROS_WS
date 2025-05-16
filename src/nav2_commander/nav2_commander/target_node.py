6eb24a4965331420faa7d64b50ae3ef26808d818
MoRan1412 <ooklibaioo@gmail.com>
2025-05-16 16:20:37 +0800

    nav2

--- src/nav2_commander/nav2_commander/target_node.py
+++ src/nav2_commander/nav2_commander/target_node.py
@@ -94,10 +94,10 @@ class SinglePointNavigation(Node):
         # 定义目标点
         target_pose = PoseStamped()
         target_pose.header.frame_id = 'map'
-        target_pose.pose.position.x = 0.4  # 目标点的 x 坐标
-        target_pose.pose.position.y = 0.2  # 目标点的 y 坐标
+        target_pose.pose.position.x = -0.4  # 目标点的 x 坐标
+        target_pose.pose.position.y = 0.0  # 目标点的 y 坐标
         target_pose.pose.orientation.z = 0.0
-        target_pose.pose.orientation.w = 3.14
+        target_pose.pose.orientation.w = 0.0
 
         # 更新时间戳
         target_pose.header.stamp = self.get_clock().now().to_msg()
