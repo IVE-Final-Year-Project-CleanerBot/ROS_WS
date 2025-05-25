69a674f60ab351e1a0825da6026815ff3cc574a7
MoRan1412 <ooklibaioo@gmail.com>
2025-05-25 18:26:19 +0800

    add sth.

--- src/driver_ros2/launch/driver.launch.py
+++ src/driver_ros2/launch/driver.launch.py
@@ -1,3 +1,4 @@
+import launch
 from launch import LaunchDescription
 from launch.substitutions import LaunchConfiguration
 from launch.actions import DeclareLaunchArgument
@@ -14,10 +15,38 @@ def generate_launch_description():
         output='screen',
     )
 
+    bottle_pickup_node = Node(
+        package='driver_ros2',
+        executable='bottle_pickup_node',
+        name='bottle_pickup_node',
+        parameters=[{'use_sim_time': use_sim_time}],
+        output='screen',
+    )
+
+    bottle_pickup_delay_node = launch.actions.TimerAction(
+        period=5.0,  # Delay in seconds before starting the bottle pickup
+        actions=[bottle_pickup_node]
+    )
+
+    bottle_place_node = Node(
+        package='driver_ros2',
+        executable='bottle_place_node',
+        name='bottle_place_node',
+        parameters=[{'use_sim_time': use_sim_time}],
+        output='screen',
+    )
+
+    bottle_place_delay_node = launch.actions.TimerAction(
+        period=10.0,  # Delay in seconds before starting the bottle place
+        actions=[bottle_place_node]
+    )
+
     return LaunchDescription([
         DeclareLaunchArgument(
             'use_sim_time',
             default_value='false',
             description='Use simulation (Gazebo) clock if true'),
         driver_node,
+        bottle_pickup_delay_node,
+        bottle_place_delay_node,
     ])
