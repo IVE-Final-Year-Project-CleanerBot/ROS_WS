96011d55134f4955539e70fec5a6fc39caf477e7
MoRan1412 <ooklibaioo@gmail.com>
2025-06-09 19:58:28 +0800

    arm fix

--- src/driver_ros2/driver_ros2/bottle_place_node.py
+++ src/driver_ros2/driver_ros2/bottle_place_node.py
@@ -8,13 +8,21 @@ class BottlePlaceNode(Node):
 
         self.arm_command_publisher = self.create_publisher(String, '/arm_command', 10)
         self.create_subscription(Bool, '/start_reached', self.start_reached_callback, 10)
+        self.create_subscription(Bool, '/gripper_has_bottle', self.gripper_callback, 10)
+
+        self.has_bottle = False
 
         self.get_logger().info("BottlePlaceNode has been started.")
 
+    def gripper_callback(self, msg):
+        self.has_bottle = msg.data
+
     def start_reached_callback(self, msg):
-        if msg.data:
+        if msg.data and self.has_bottle:
             self.arm_command_publisher.publish(String(data="move_to_place"))
             self.get_logger().info("Bottle place completed.")
+        elif msg.data and not self.has_bottle:
+            self.get_logger().warn("Start reached, but no bottle in gripper. Skip placing.")
 
 def main(args=None):
     rclpy.init(args=args)
